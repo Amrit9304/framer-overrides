fe70c86c3a194ad7654cdaa9337b74d3b311cc8e
Amrit9304 <91752236+Amrit9304@users.noreply.github.com>
2024-02-09 20:57:59 +0530

    Update MagneticEffectOnHover.tsx

--- src/MagneticEffectOnHover.tsx
+++ src/MagneticEffectOnHover.tsx
@@ -2,11 +2,9 @@ import React, { useRef, useState } from "react"
 import type { ComponentType } from "react"
 import { motion } from "framer-motion"
 
-interface EnlargeOnHoverProps {}
+interface OnHoverProps {}
 
-export function withEnlargeOnHover(
-    Component: ComponentType
-): ComponentType<EnlargeOnHoverProps> {
+export function OnHover(Component: ComponentType): ComponentType<OnHoverProps> {
     return (props) => {
         const ref = useRef(null)
         const [position, setPosition] = useState({ x: 0, y: 0 })
@@ -16,10 +14,20 @@ export function withEnlargeOnHover(
             const { left, top, width, height } =
                 event.target.getBoundingClientRect()
 
-            const value = 0.4 //change it according to how much movement you want, 1 is default
-            const middleX = (clientX - (left + width / 2)) * value
-            const middleY = (clientY - (top + height / 2)) * value
-            setPosition({ x: middleX, y: middleY })
+            // Adjust the stickiness factor
+            const stickiness = 0.3
+            
+            const centerX = left + width / 2
+            const centerY = top + height / 2
+            const distanceX = clientX - centerX
+            const distanceY = clientY - centerY
+
+            const stickyX =
+                distanceX * Math.log(Math.abs(distanceX) + 1) * stickiness
+            const stickyY =
+                distanceY * Math.log(Math.abs(distanceY) + 1) * stickiness
+
+            setPosition({ x: stickyX, y: stickyY })
         }
 
         const reset = () => {
